b64c44b4e155cea9dc8c2f7630e5a237105d0db8
Chris Cowley <chris@chriscowley.me.uk>
2014-12-11 22:11:37 +0100

    Seperated out config to simplify mongo variables

--- alerter/__init__.py
+++ alerter/__init__.py
@@ -2,5 +2,10 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 from alerter import views
 
+from flask.ext.pymongo import PyMongo
+
+mongo = PyMongo(app)
+app.config.from_pyfile('alerter.cfg')
+
 if __name__ == '__main__':
     app.run(debug=True)
